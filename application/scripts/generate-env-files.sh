5883de86a8faa316bb8ad2d2805a3735b7eb83b7
Mirkesx <cmarcos46@gmail.com>
2022-09-09 10:50:29 +0200

    feat: get config values from aws-sdk instead of from hardcoded files

---
+++ application/scripts/generate-env-files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+API_GATEWAY_URL=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud-Systems-IoT-ApiURL'].Value" --output text )
+COGNITO_URL=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud-Systems-IoT-CognitoUserPoolProviderURL'].Value" --output text )
+COGNITO_APP_CLIENT=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud-Systems-IoT-CognitoUserPoolClientId'].Value" --output text )
+
+echo "export const api_gateway = {
+    url: '${API_GATEWAY_URL}',
+    app_client: '5nfdl5n8a1aa3pts1hqsu9oefi',
+    domain: 'cloud-systems-iot-project-640339490701'
+}; " > ./src/config/api_gateway.jsx
+
+echo "export const cognito = {
+    cognito_url: '${COGNITO_URL}/',
+    app_client: '${COGNITO_APP_CLIENT}'
+};" > ./src/config/cognito.jsx
