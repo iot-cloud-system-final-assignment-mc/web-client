fd3ecf9d51e5c5afc988b73d7c714d7ddff2adf8
Mirkesx <cmarcos46@gmail.com>
2022-09-12 11:57:21 +0200

    feat: add logic to prevent actions when modifying an order

--- application/src/pages/orders.jsx
+++ application/src/pages/orders.jsx
@@ -13,9 +13,47 @@ const OrdersPage = () => {
     const columns = ["order_id", "product_id", "username", "quantity", "total_price", "status", "updated_at"];
 
     useEffect(() => {
+        getOrders();
+    }, []);
+
+    const getOrders = () => {
         OrdersApi.getOrders().then(response => {
             //console.log(response);
             const data = response.map((order) => {
+                const buttons = [];
+                if(order.status !== "cancelled" && order.status !== "delivered") {
+                if(order.status === "pending") 
+                    buttons.push(
+                        {
+                            "icon": "send",
+                            "onClick": "actionOne",
+                            "args": { ...order, status: 'shipped'}
+                        }
+                    );
+                if(order.status === "shipped")
+                    buttons.push(
+                        {
+                            "icon": "check",
+                            "onClick": "actionTwo",
+                            "args": { ...order, status: 'delivered'}
+                        }
+                    );
+                buttons.push(
+                    {
+                        "icon": "trash",
+                        "onClick": "actionThree",
+                        "args": { ...order, status: 'cancelled'}
+                    }
+                );
+                } else {
+                    buttons.push(
+                        {
+                            "icon": "plus",
+                            "onClick": "actionFour",
+                            "args": { ...order, status: 'pending'}
+                        }
+                    );
+                }
                 return {
                     order_id: {
                         value: order.order_id,
@@ -39,18 +77,52 @@ const OrdersPage = () => {
                     updated_at: {
                         value: order.updated_at
                     },
-                    buttons: []
+                    buttons: buttons
                 };
             });
             setValues(data);
         }).catch(error => {
             console.log(error);
         });
-    }, []);
+    };
+
+    const cloneOrder = (args) => {
+        delete args.order_id;
+        OrdersApi.upsertOrder(args).then(response => {
+            getOrders();
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    const setShipped = (args) => {
+        OrdersApi.upsertOrder(args).then(response => {
+            getOrders();
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    const setDelivered = (args) => {
+        OrdersApi.upsertOrder(args).then(response => {
+            getOrders();
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    const setCancelled = (args) => {
+        OrdersApi.upsertOrder(args).then(response => {
+            getOrders();
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
     return (
         <DashboardLayout>
             <h1 style={{textAlign: "center"}}>Orders</h1>
-            <BaseTable columns={columns} values={values} buttons={false}/><br/>
+            <BaseTable columns={columns} values={values} buttons={true} actionOne={setShipped} actionTwo={setDelivered} actionThree={setCancelled} actionFour ={cloneOrder}/><br/>
             {/* <div><button onClick={() => history.push("/order/add")}>Add</button></div> */}
         </DashboardLayout>
     )
