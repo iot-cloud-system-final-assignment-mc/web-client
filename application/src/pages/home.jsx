51830bceca9795bd4e1d8919b122e8385f2a3feb
Mirkesx <cmarcos46@gmail.com>
2022-09-13 13:18:35 +0200

    feat: homepage now greets the user

--- application/src/pages/home.jsx
+++ application/src/pages/home.jsx
@@ -2,8 +2,12 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import {DashboardLayout} from '../components/Layout';
+import authUtils from '../utils/authUtils';
 
 const HomePage = () => {
+  const payload = authUtils.getIdTokenPayload();
+  const isAdmin = payload['cognito:groups'] && payload['cognito:groups'].includes('Admin');
+  const username = payload['cognito:username'];
   const history = useHistory();
   useEffect(() => {
     if(window.location.hash.length > 0) {
@@ -12,7 +16,8 @@ const HomePage = () => {
   } , [history]);
   return (
     <DashboardLayout>
-      <h2>Home Page</h2>
+      <p className="title">{isAdmin ? "Welcome back Products App!" : `Welcome to Products App, ${username}!`}</p>
+      <p className="body">{isAdmin ? "You may proceed adding new products or managing the orders." : `Hey ${username}. If you'd like you can proceed buying a new product! Also, please check your orders to see the status of your active orders!`}</p>
     </DashboardLayout>
   )
 }
