2443b483c2626441e5475e6340378eb5b16a8590
Mirkesx <cmarcos46@gmail.com>
2022-09-26 13:48:56 +0200

    feat: create config folder if it doesn't exist

--- application/scripts/generate-env-files.sh
+++ application/scripts/generate-env-files.sh
@@ -4,6 +4,9 @@ API_GATEWAY_URL=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud
 COGNITO_URL=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud-Systems-IoT-CognitoUserPoolProviderURL'].Value" --output text )
 COGNITO_APP_CLIENT=$(aws cloudformation list-exports --query "Exports[?Name=='Cloud-Systems-IoT-CognitoUserPoolClientId'].Value" --output text )
 
+ls
+mkdir ./src/config
+
 echo "export const api_gateway = {
     url: '${API_GATEWAY_URL}',
     app_client: '5nfdl5n8a1aa3pts1hqsu9oefi',
